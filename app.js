7d06bc3eab3f7c5a61b7c1103fb7b6c462e21922
chenhao <302835677@qq.com>
2017-07-18 23:20:21 +0800

    搭建基础聊天室

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '/static')));
 app.use((req, res) => {
@@ -12,8 +12,15 @@ app.use((req, res) => {
 const server = app.listen(port, () => {
   console.log('chat is on port ' + port + '!');
 });
-
+let messages = [];
 const io = require('socket.io').listen(server);
 io.sockets.on('connection', (socket) => {
-  socket.emit('connected');
+  socket.on('getAllMessages', () => {
+    socket.emit('allMessages', messages);
+  });
+  socket.on('createMessage', (message) => {
+    messages.push(message);
+    console.log(messages);
+    io.sockets.emit('messageAdded', message);
+  })
 });
